dce6f3faa6ca674d4e0982001f044f08befb9ff7
signal86 <parathenium@proton.me>
2025-04-04 23:09:33 -0500

    locking

--- main.go
+++ main.go
@@ -6,21 +6,29 @@ import (
 	// "fmt"
 )
 
-func collatz(n uint64, cache *sync.Map) bool {
+func collatz(n uint64, cache *sync.Map, wg *sync.WaitGroup) {
+	defer wg.Done()
 	original := n
+	var cache2 []uint64
 	for n != 1 {
 		if _, ok := cache.Load(n); ok {
+			for _, i := range cache2 {
+				cache.Store(i, true)
+			}
 			cache.Store(original, true)
-			break
+			return
 		}
+		cache2 = append(cache2, n)
 		if n % 2 == 0 {
 			n /= 2
 		} else {
 			n = 3 * n + 1
 		}
 	}
+	for _, i := range cache2 {
+		cache.Store(i, true)
+	}
 	cache.Store(original, true)
-	return true
 }
 
 func main() {
@@ -28,7 +36,10 @@ func main() {
 	// cache := make(map[uint64]bool)
 	// var mutx sync.Mutex
 	var cache sync.Map
+	var wg sync.WaitGroup
 	for n := uint64(1); n < 5000000001; n++ {
-		go collatz(n, &cache)
+		wg.Add(1)
+		go collatz(n, &cache, &wg)
 	}
+	wg.Wait()
 }
